a0d71e8beb90e2015c69ecdfa2d070125f8af976
Anastasiia Popova <stacypopova123@fb.com>
2022-06-13 14:21:38 -0700

    saving post added

--- app/src/main/java/com/example/parstagram31/MainActivity.java
+++ app/src/main/java/com/example/parstagram31/MainActivity.java
@@ -7,22 +7,59 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.parstagram31.Models.Post;
+import com.example.parstagram31.databinding.ActivityMainBinding;
 import com.parse.LogOutCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MAIN ACTIVITY";
 
+    ActivityMainBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        Post.queryPosts();
+        binding.btnsubmit.setOnClickListener(v -> {
+            if(binding.etDescription.getText() == null || binding.etDescription.getText().toString().equals("")) {
+                Toast.makeText(MainActivity.this, "no description", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            savePost(binding.etDescription.getText().toString(), ParseUser.getCurrentUser());
+        });
+
+        binding.btnTakePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private void savePost(String description, ParseUser currentUser) {
+        Post post = new Post();
+        post.setDescription(description);
+        post.setUser(currentUser);
+        post.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if(e != null) {
+                    Toast.makeText(MainActivity.this, "saving failed", Toast.LENGTH_SHORT);
+                    Log.i(TAG, "fail: " + e);
+                    return;
+                }
+                binding.etDescription.setText(null);
+            }
+        });
     }
 
     @Override
