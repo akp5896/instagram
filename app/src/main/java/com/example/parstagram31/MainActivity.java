fbab56d5aab8c390ee6ed1351b63415ff7b9c2ff
Anastasiia Popova <stacypopova123@fb.com>
2022-06-13 13:44:15 -0700

    logout added

--- app/src/main/java/com/example/parstagram31/MainActivity.java
+++ app/src/main/java/com/example/parstagram31/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.parstagram31;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.parse.LogOutCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MAIN ACTIVITY";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.logout) {
+            ParseUser.logOutInBackground(new LogOutCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if(e != null) {
+                        Log.i(TAG, "Logout failed");
+                        return;
+                    }
+                    finish();
+                }
+            });
+
+        }
+        return true;
     }
 }
