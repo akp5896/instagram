4530cf16b001070bc59565ca87e32ca8fbd1fca3
Anastasiia Popova <stacypopova123@fb.com>
2022-06-14 15:39:47 -0700

    comment layout fixed

--- app/src/main/java/com/example/parstagram31/Adapter/CommentAdapter.java
+++ app/src/main/java/com/example/parstagram31/Adapter/CommentAdapter.java
@@ -54,7 +54,7 @@ public class CommentAdapter extends RecyclerView.Adapter<CommentAdapter.ViewHold
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.item_post, parent, false);
+        View view = LayoutInflater.from(context).inflate(R.layout.item_comment, parent, false);
         return new ViewHolder(view);
     }
 
